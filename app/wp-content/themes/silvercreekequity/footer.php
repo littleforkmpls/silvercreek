f300fdf41abe8f1ef78a5d96d31f93af02cd7d26
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-04 14:32:42 -0600

    setup footer copyright

--- app/wp-content/themes/silvercreekequity/footer.php
+++ app/wp-content/themes/silvercreekequity/footer.php
@@ -1,9 +1,14 @@
 
             <div class="site__footer" role="contentinfo">
-                <div class="tier">
+                <div class="tier tier--peacock-dark-gradient">
                     <div class="wrapper">
-
-
+                        <div class="footer">
+                            <div class="footer__legal">
+                                <small class="txt txt--size-xs txt--color-white txt--family-sans">
+                                    Copyright &copy; <?php echo date('Y'); ?> Silver Creek Equity. All Rights Reserved.
+                                </small>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
