a31a3499b9cbd1be3509d1a6e5f9ed80858e8d7e
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-05 22:51:34 -0600

    masthead markup start

--- app/wp-content/themes/silvercreekequity/header.php
+++ app/wp-content/themes/silvercreekequity/header.php
@@ -9,10 +9,43 @@
     <body>
         <div class="site">
             <div class="site__masthead" role="banner">
-                <div class="tier">
+                <div class="tier tier--peacock-dark-gradient">
                     <div class="wrapper">
-
-
+                        <div class="masthead">
+                            <div class="masthead__brand">
+                                <div class="logo">
+                                    <img src="" alt="Silver Creek Equity" />
+                                </div>
+                            </div>
+                            <div class="masthead__nav">
+                                <div class="nav" role="nav">
+                                    <ul id="primaryNav" class="nav__list">
+                                        <li>
+                                            <a href="#">About</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Projects</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">News</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="masthead__utils">
+                                <div class="utils">
+                                    <div class="utils__contact">
+                                        <a href="#">612-282-7053</a>
+                                    </div>
+                                    <div class="utils__portal"
+                                        <a href="#">Login</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
