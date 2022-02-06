9f1a74d579fc181948c3b483eaa7b42fddd667cd
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-05 22:51:17 -0600

    stubbing in dark peacock tier with footer legal copy

--- app/wp-content/themes/silvercreekequity/footer.php
+++ app/wp-content/themes/silvercreekequity/footer.php
@@ -4,7 +4,7 @@
                     <div class="wrapper">
                         <div class="footer">
                             <div class="footer__legal">
-                                <small class="txt txt--size-xs txt--color-white txt--family-sans">
+                                <small class="txt txt--size-xs txt--family-sans">
                                     Copyright &copy; <?php echo date('Y'); ?> Silver Creek Equity. All Rights Reserved.
                                 </small>
                             </div>
