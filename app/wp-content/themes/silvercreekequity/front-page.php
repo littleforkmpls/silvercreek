f40feb7a198ea6437c1f13bce58818b98fd916ea
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-12 08:36:25 -0600

    more homepage

--- app/wp-content/themes/silvercreekequity/front-page.php
+++ app/wp-content/themes/silvercreekequity/front-page.php
@@ -7,11 +7,12 @@
     <div class="tier tier--peacock-dark-media" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/content/hero-home.jpg')">
         <div class="wrapper">
             <div class="section">
+
                 <div class="hero">
                     <div class="hero__hd">
-                        <h2 class="txt txt--hero">
+                        <h1 class="txt txt--hero">
                             Let&rsquo;s start<br/> building wealth<br class="break break--whenSmall"> together.
-                        </h2>
+                        </h1>
                     </div>
                     <div class="hero__bd">
                         <p class="txt txt--heroSub">We&rsquo;re private equity managers specializing in multifamily and senior living communities from concept to completion.</p>
@@ -23,6 +24,7 @@
                         </a>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
@@ -33,6 +35,42 @@
         <div class="tier__bd">
             <div class="wrapper">
                 <div class="slab">
+
+                    <div class="section">
+                        <div class="section__hd">
+                            <div class="title">
+                                <h2 class="txt txt--title">Projects</h2>
+                            </div>
+                        </div>
+                        <div class="section__bd">
+                            <div style="text-align: center">
+                                <h3 class="txt txt--hdg">Find the perfect project for your investment dollar.</h3>
+                                <p>From market rate apartments to a variety of unique senior living properties, our vast project portfolio gives you acres of investment options.</p>
+                                <div>Cards go here</div>
+                            </div>
+                        </div>
+                        <div class="section__ft">
+                            <a class="btn" href="">
+                                <span class="btn__txt">All Projects</span>
+                                <span class="btn__icon"></span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="section">
+                        <div class="section__hd">
+                            <div class="title">
+                                <h2 class="txt txt--title">Our Process</h2>
+                            </div>
+                        </div>
+                        <div class="section__bd">
+                            <div style="text-align: center">
+                                <h3 class="txt txt--hdg">Our process creates low risk and high returns.</h3>
+                                <p>To give each project the best chance to succeed, we put our development experience to work and lean on the support of other experts in construction, design, project management and market study analysis.</p>
+                                <div>Cards go here</div>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
