a7d6c4f2496fa597d657409160cbd30b118f9a22
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-15 12:38:58 -0600

    making all aspects of text, logos, links, etc configurable and dyanamic via ACF for header and footer

--- app/wp-content/themes/silvercreekequity/footer.php
+++ app/wp-content/themes/silvercreekequity/footer.php
@@ -4,30 +4,43 @@
                     <div class="wrapper">
                         <div class="footer">
                             <div class="footer__social">
-                                <ul class="footer__social__list">
-                                    <li>
-                                        <a class="hasSocialIcon" href="#linkedin">
-                                            <i class="socialIcon socialIcon--isLight socialIcon--linkedin" aria-hidden="true"></i>
-                                            <span class="isVisuallyHidden">LinkedIn</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="hasSocialIcon" href="#facebook">
-                                            <i class="socialIcon socialIcon--isLight socialIcon--facebook" aria-hidden="true"></i>
-                                            <span class="isVisuallyHidden">Facebook</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="hasSocialIcon" href="#twitter">
-                                            <i class="socialIcon socialIcon--isLight socialIcon--twitter" aria-hidden="true"></i>
-                                            <span class="isVisuallyHidden">Twitter</span>
-                                        </a>
-                                    </li>
-                                </ul>
+                                <?php
+                                    $linkedIn_url = get_field('socialmedia_linkedin_url', 'option');
+                                    $facebook_url = get_field('socialmedia_facebook_url', 'option');
+                                    $twitter_url = get_field('socialmedia_twitter_url', 'option');
+                                ?>
+                                <?php if ($linkedIn_url || $facebook_url || $twitter_url) : ?>
+                                    <ul class="footer__social__list">
+                                        <?php if ($linkedIn_url) : ?>
+                                        <li>
+                                            <a class="hasSocialIcon" href="<?php echo $linkedIn_url; ?>">
+                                                <i class="socialIcon socialIcon--isLight socialIcon--linkedin" aria-hidden="true"></i>
+                                                <span class="isVisuallyHidden">LinkedIn</span>
+                                            </a>
+                                        </li>
+                                        <?php endif; ?>
+                                        <?php if ($facebook_url) : ?>
+                                        <li>
+                                            <a class="hasSocialIcon" href="<?php echo $facebook_url; ?>">
+                                                <i class="socialIcon socialIcon--isLight socialIcon--facebook" aria-hidden="true"></i>
+                                                <span class="isVisuallyHidden">Facebook</span>
+                                            </a>
+                                        </li>
+                                        <?php endif; ?>
+                                        <?php if ($twitter_url) : ?>
+                                        <li>
+                                            <a class="hasSocialIcon" href="<?php echo $twitter_url; ?>">
+                                                <i class="socialIcon socialIcon--isLight socialIcon--twitter" aria-hidden="true"></i>
+                                                <span class="isVisuallyHidden">Twitter</span>
+                                            </a>
+                                        </li>
+                                        <?php endif; ?>
+                                    </ul>
+                                <?php endif; ?>
                             </div>
                             <div class="footer__legal">
                                 <small class="txt txt--legal">
-                                    &copy; <?php echo date('Y'); ?> Silver Creek Equity. All Rights Reserved.
+                                    &copy; <?php echo date('Y'); ?> <?php the_field('global_copyright_text', 'option'); ?>
                                 </small>
                             </div>
                         </div>
--- app/wp-content/themes/silvercreekequity/header.php
+++ app/wp-content/themes/silvercreekequity/header.php
@@ -14,7 +14,7 @@
                         <div class="masthead" role="navigation">
                             <div class="masthead__brand">
                                 <a href="<?php echo site_url(); ?>">
-                                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/silver-creek-equity-logo.svg" alt="Silver Creek Equity" />
+                                    <img src="<?php the_field('global_logo', 'option'); ?>" alt="<?php echo get_bloginfo('name'); ?>" />
                                 </a>
                             </div>
                             <div class="masthead__toggle">
@@ -42,38 +42,51 @@
                                 <div class="masthead__nav__secondary">
                                     <ul class="masthead__nav__secondary__list">
                                         <li>
-                                            <a href="#phone">612-282-7053</a>
+                                            <a href="tel:+1-<?php echo trim(preg_replace('/[^0-9] -/', '', get_field('global_phone_number', 'option'))); ?>"><?php the_field('global_phone_number', 'option'); ?></a>
                                         </li>
                                         <li>
-                                            <a href="#login">Login</a>
+                                            <a href="<?php the_field('global_portal_link', 'option'); ?>"><?php the_field('global_portal_link_text', 'option'); ?></a>
                                         </li>
                                     </ul>
                                 </div>
                                 <div class="masthead__nav__social">
-                                    <ul class="masthead__nav__social__list">
-                                        <li>
-                                            <a class="hasSocialIcon" href="#linkedin">
-                                                <i class="socialIcon socialIcon--linkedin" aria-hidden="true"></i>
-                                                <span class="isVisuallyHidden">LinkedIn</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="hasSocialIcon" href="#facebook">
-                                                <i class="socialIcon socialIcon--facebook" aria-hidden="true"></i>
-                                                <span class="isVisuallyHidden">Facebook</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="hasSocialIcon" href="#twitter">
-                                                <i class="socialIcon socialIcon--twitter" aria-hidden="true"></i>
-                                                <span class="isVisuallyHidden">Twitter</span>
-                                            </a>
-                                        </li>
-                                    </ul>
+                                    <?php
+                                        $linkedIn_url = get_field('socialmedia_linkedin_url', 'option');
+                                        $facebook_url = get_field('socialmedia_facebook_url', 'option');
+                                        $twitter_url = get_field('socialmedia_twitter_url', 'option');
+                                    ?>
+                                    <?php if ($linkedIn_url || $facebook_url || $twitter_url) : ?>
+                                        <ul class="masthead__nav__social__list">
+                                            <?php if ($linkedIn_url) : ?>
+                                            <li>
+                                                <a class="hasSocialIcon" href="<?php echo $linkedIn_url; ?>">
+                                                    <i class="socialIcon socialIcon--linkedin" aria-hidden="true"></i>
+                                                    <span class="isVisuallyHidden">LinkedIn</span>
+                                                </a>
+                                            </li>
+                                            <?php endif; ?>
+                                            <?php if ($facebook_url) : ?>
+                                            <li>
+                                                <a class="hasSocialIcon" href="<?php echo $facebook_url; ?>">
+                                                    <i class="socialIcon socialIcon--facebook" aria-hidden="true"></i>
+                                                    <span class="isVisuallyHidden">Facebook</span>
+                                                </a>
+                                            </li>
+                                            <?php endif; ?>
+                                            <?php if ($twitter_url) : ?>
+                                            <li>
+                                                <a class="hasSocialIcon" href="<?php echo $twitter_url; ?>">
+                                                    <i class="socialIcon socialIcon--twitter" aria-hidden="true"></i>
+                                                    <span class="isVisuallyHidden">Twitter</span>
+                                                </a>
+                                            </li>
+                                            <?php endif; ?>
+                                        </ul>
+                                    <?php endif; ?>
                                 </div>
                                 <div class="masthead__nav__legal">
                                     <small class="txt txt--legal">
-                                        &copy; <?php echo date('Y'); ?> Silver Creek Equity. All Rights Reserved.
+                                        &copy; <?php echo date('Y'); ?> <?php the_field('global_copyright_text', 'option'); ?>
                                     </small>
                                 </div>
                             </div>
