8ce2bd29609b18685d00e3345e78bb24cf8e0e66
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-15 10:35:53 -0600

    remove head cleanup from functions.php

--- app/wp-content/themes/silvercreekequity/functions.php
+++ app/wp-content/themes/silvercreekequity/functions.php
@@ -35,19 +35,7 @@ add_action(
 /* ====================================================================================================
    Cleanup the Head
 ==================================================================================================== */
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'wp_resource_hints', 2 );
-remove_action('wp_head', 'feed_links', 2);
-remove_action('wp_head', 'rest_output_link_wp_head');
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wp_generator');
 
-add_action('wp_print_styles', function (): void {
-    wp_dequeue_style('wp-block-library');
-    wp_dequeue_style('wp-block-library-theme');
-});
 
 /* ====================================================================================================
    Disable Block Editor
