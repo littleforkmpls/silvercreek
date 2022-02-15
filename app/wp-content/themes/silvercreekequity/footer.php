82c031f38cec9856c888a8e20997806518852c33
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-15 08:31:53 -0600

    add social icons to footer

--- app/wp-content/themes/silvercreekequity/footer.php
+++ app/wp-content/themes/silvercreekequity/footer.php
@@ -3,6 +3,28 @@
                 <div class="tier tier--peacock-dark-gradient">
                     <div class="wrapper">
                         <div class="footer">
+                            <div class="footer__social">
+                                <ul class="footer__social__list">
+                                    <li>
+                                        <a class="hasSocialIcon" href="#linkedin">
+                                            <i class="socialIcon socialIcon--isLight socialIcon--linkedin"" aria-hidden="true"></i>
+                                            <span class="isVisuallyHidden">LinkedIn</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="hasSocialIcon" href="#facebook">
+                                            <i class="socialIcon socialIcon--isLight socialIcon--facebook"" aria-hidden="true"></i>
+                                            <span class="isVisuallyHidden">Facebook</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="hasSocialIcon" href="#twitter">
+                                            <i class="socialIcon socialIcon--isLight socialIcon--twitter"" aria-hidden="true"></i>
+                                            <span class="isVisuallyHidden">Twitter</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                             <div class="footer__legal">
                                 <small class="txt txt--legal">
                                     &copy; <?php echo date('Y'); ?> Silver Creek Equity. All Rights Reserved.
