d9cf89cdef752b5827c4304880f71dba9e812b87
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-19 12:49:14 -0600

    setup standard page template to use ACF components

--- app/wp-content/themes/silvercreekequity/page.php
+++ app/wp-content/themes/silvercreekequity/page.php
@@ -3,11 +3,40 @@
 <?php get_header(); ?>
 <div class="site__main" role="main">
 
+    <?php
+        // loop through components and display hero
+        if(have_rows('page_components')):
+            while (have_rows('page_components')) : the_row();
+                if(get_row_layout() == 'hero'):
+                    get_template_part('includes/component-hero');
+                endif;
+            endwhile;
+        endif;
+    ?>
+
     <div class="tier">
         <div class="wrapper">
-            <div class="section">
-                <h1>General Page</h1>
-            </div>
+
+            <?php
+                // loop through components and display all except hero
+                if(have_rows('page_components')):
+                    while (have_rows('page_components')) : the_row();
+                        if(get_row_layout() == 'featured_projects_slider'):
+                            get_template_part('includes/component-featured-projects-slider');
+                        elseif(get_row_layout() == 'process_slider'):
+                            get_template_part('includes/component-process-slider');
+                        endif;
+                    endwhile;
+                endif;
+            ?>
+
+        </div>
+    </div>
+
+    <?php // contact form ?>
+    <div class="tier tier--peacock-dark">
+        <div class="wrapper">
+            <?php get_template_part('includes/form-contact'); ?>
         </div>
     </div>
 
