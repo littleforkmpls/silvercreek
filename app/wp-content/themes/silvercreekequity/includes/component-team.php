3eeb20dbf24a482310251c6ae2725afd0e305159
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-22 11:14:43 -0600

    team component for about us page

--- app/wp-content/themes/silvercreekequity/includes/component-team.php
+++ app/wp-content/themes/silvercreekequity/includes/component-team.php
@@ -42,4 +42,42 @@
             <?php endif; ?>
         </div>
     </div>
+    <div class="section__team">
+        <div class="yearbook">
+            <?php while(have_rows('team_members')) : the_row(); ?>
+                <?php
+                    $team_member_name       = get_sub_field('team_member_name');
+                    $team_member_job_title  = get_sub_field('team_member_job_title');
+                    $team_member_bio        = get_sub_field('team_member_bio');
+                    $team_member_image_id   = get_sub_field('team_member_headshot');
+
+                    $team_member_image_url   = wp_get_attachment_image_url($team_member_image_id, 'bio-headshot');
+                ?>
+                <div class="yearbook__item">
+                    <div class="frame">
+                        <div class="frame__media">
+                            <img class="isBlock" width="250" height="300" src="<?php echo $team_member_image_url; ?>" alt="<?php echo $team_member_name; ?>" />
+                        </div>
+                        <div class="frame__content">
+                            <div class="frame__content__hd">
+                                <h4 class="txt txt--hdg txt--color-brand-dark">
+                                    <?php echo $team_member_name; ?>
+                                </h4>
+                            </div>
+                            <div class="frame__content__subhd">
+                                <div class="txt txt--body txt--color-brand-peacock">
+                                    <?php echo $team_member_job_title; ?>
+                                </div>
+                            </div>
+                            <div class="frame__content__bd">
+                                <div>
+                                    <?php echo $team_member_bio; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
 </div>
