cbcb14e34155db5171463bc1a81ecdd6a6c17c2e
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-23 09:26:50 -0600

    force archive and single pages to 404 until news gets setup

--- app/wp-content/themes/silvercreekequity/single.php
+++ app/wp-content/themes/silvercreekequity/single.php
@@ -1,15 +1,8 @@
 <?php // INDIVIDUAL POST PAGE ?>
 
-<?php get_header(); ?>
-<div class="site__main" role="main">
-
-    <div class="tier">
-        <div class="wrapper">
-            <div class="section section--noDivider">
-                <h1>Post Page</h1>
-            </div>
-        </div>
-    </div>
-
-</div>
-<?php get_footer(); ?>
+<?php
+    global $wp_query;
+    $wp_query->set_404();
+    status_header( 404 );
+    get_template_part( 404 ); exit();
+?>
