53217886bb87187077a8f92c23fbf1e3023dd371
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-23 13:07:05 -0600

    add archive and single templates for projects custom post type

---
+++ app/wp-content/themes/silvercreekequity/archive-scequity_projects.php
@@ -0,0 +1,15 @@
+<?php // ARCHIVE PAGE for PROJECTS ?>
+
+<?php get_header(); ?>
+<div class="site__main" role="main">
+
+    <div class="tier">
+        <div class="wrapper">
+            <div class="section section--noDivider">
+                <h1>Project Archive Page</h1>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php get_footer(); ?>
---
+++ app/wp-content/themes/silvercreekequity/single-scequity_projects.php
@@ -0,0 +1,15 @@
+<?php // INDIVIDUAL PROJECT PAGE ?>
+
+<?php get_header(); ?>
+<div class="site__main" role="main">
+
+    <div class="tier">
+        <div class="wrapper">
+            <div class="section section--noDivider">
+                <h1>Single Project Page</h1>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php get_footer(); ?>
