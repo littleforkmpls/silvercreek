4d71faf6c7d8d2c5d311d75f5bc159d56db88de3
Anthony Ticknor <tony.ticknor@gmail.com>
2022-03-01 19:27:25 -0600

    update 404 and message template designs

--- app/wp-content/themes/silvercreekequity/404.php
+++ app/wp-content/themes/silvercreekequity/404.php
@@ -5,17 +5,28 @@
     <div class="site__main__inner">
 
         <div class="placard">
-            <div class="placard__inner">
-                <div class="placard__hd">
-                    <h1 class="txt txt--hero">
-                        <?php the_field('404_page_headline', 'option'); ?>
-                    </h1>
+            <div class="placard__title">
+                <div class="placard__title__inner">
+                    <div class="txt txt--label txt--color-brand-grey">
+                        404 Error
+                    </div>
                 </div>
-                <div class="placard__bd">
-                    <p class="txt txt--body2">
-                        <?php the_field('404_page_blurb', 'option'); ?>
-                    </p>
-                </div>
+            </div>
+            <div class="placard__hd">
+                <h1 class="txt txt--hero">
+                    <?php the_field('404_page_headline', 'option'); ?>
+                </h1>
+            </div>
+            <div class="placard__bd">
+                <p class="txt txt--heroSub">
+                    <?php the_field('404_page_blurb', 'option'); ?>
+                </p>
+            </div>
+            <div class="placard__cta">
+                <a class="btn" href="<?php echo site_url(); ?>">
+                    <span class="btn__txt">Home</span>
+                    <span class="btn__icon"></span>
+                </a>
             </div>
         </div>
 
--- app/wp-content/themes/silvercreekequity/page_message.php
+++ app/wp-content/themes/silvercreekequity/page_message.php
@@ -5,17 +5,28 @@
     <div class="site__main__inner">
 
         <div class="placard">
-            <div class="placard__inner">
-                <div class="placard__hd">
-                    <h1 class="txt txt--hero">
-                        <?php the_title(); ?>
-                    </h1>
-                </div>
-                <div class="placard__bd">
-                    <div class="txt txt--body2">
-                        <?php the_field('short_message_text'); ?>
+            <div class="placard__title">
+                <div class="placard__title__inner">
+                    <div class="txt txt--label txt--color-brand-grey">
+                        Contact
                     </div>
                 </div>
+            </div>
+            <div class="placard__hd">
+                <h1 class="txt txt--hero">
+                    <?php the_title(); ?>
+                </h1>
+            </div>
+            <div class="placard__bd">
+                <div class="txt txt--heroSub">
+                    <?php the_field('short_message_text'); ?>
+                </div>
+            </div>
+            <div class="placard__cta">
+                <a class="btn" href="<?php echo site_url(); ?>">
+                    <span class="btn__txt">Home</span>
+                    <span class="btn__icon"></span>
+                </a>
             </div>
         </div>
 
