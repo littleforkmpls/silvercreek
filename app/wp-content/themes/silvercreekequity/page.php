4f71f33d5ade81003e862aceddf97b388b980dbb
Anthony Ticknor <tony.ticknor@gmail.com>
2022-03-03 14:12:05 -0600

    shuffle hero location so on the homepage it can precede the mastehad

--- app/wp-content/themes/silvercreekequity/page.php
+++ app/wp-content/themes/silvercreekequity/page.php
@@ -1,6 +1,27 @@
 <?php // PAGEES: HOME, ABOUT, GENERAL, ETC ?>
 
 <?php get_header(); ?>
+
+<?php
+    if(is_front_page()) {
+        $hero_id = 'site__hero--home';
+    } else {
+        $hero_id ='';
+    }
+?>
+
+<div class="site__hero <?php echo $hero_id; ?>">
+    <?php
+        if(have_rows('page_components')):
+            while (have_rows('page_components')) : the_row();
+                if(get_row_layout() == 'hero'):
+                    get_template_part('includes/component-hero');
+                endif;
+            endwhile;
+        endif;
+    ?>
+</div>
+
 <div class="site__main" role="main">
 
     <?php // page title for SEO & accessibility always present in the h1 ?>
@@ -18,9 +39,7 @@
         // loop through components
         if(have_rows('page_components')):
             while (have_rows('page_components')) : the_row();
-                if(get_row_layout() == 'hero'):
-                    get_template_part('includes/component-hero');
-                elseif(get_row_layout() == 'featured_projects_slider'):
+                if(get_row_layout() == 'featured_projects_slider'):
                     get_template_part('includes/component-featured-projects-slider');
                 elseif(get_row_layout() == 'process_slider'):
                     get_template_part('includes/component-process-slider');
